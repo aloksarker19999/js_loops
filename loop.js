87570d0279124f2b101ad6fd63bded7ebbbf4c09
aloksarker19999 <aloksarker360@gmail.com>
2026-06-30 01:49:07 +0600

    init

---
+++ loop.js
@@ -0,0 +1,47 @@
+//JavaScript Loop
+
+let car = ['BMW', 'Volvo', 'Saab', 'Ford', 'Fiat', 'Audi'];
+car.push('Alok');
+// console.log(car[0]);
+// console.log(car[1]);
+// console.log(car[2]);
+
+// for (let i = 0; i < car.length; i++) {
+//     console.log(car[i]);
+// }
+
+
+//For In Loop
+
+let person = {
+    name: 'Alok',
+    age: 30,
+    country: 'Bangladesh',
+    job: 'Web Developer',
+}
+
+// for (let i in person) {
+//     let capLetter = i.charAt(0).toUpperCase() + i.slice(1);
+//     console.log(capLetter + ': ' + person[i]);
+// }
+
+
+//ForEach Method
+
+// car.forEach(function (i) {
+//     console.log(i);
+// });
+
+
+//For Of Loop
+for (let i of car) {
+    console.log(i);
+}
+
+
+//While Loop
+let num = 0;
+while (num < 10) {
+    console.log(num);
+    num++;
+}
